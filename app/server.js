62cfe05077d6c94168a85473eff19a6927f3cecc
Louay122 <louay.gourrida@esprit.tn>
2022-03-24 03:35:34 -0700

    testingwebhook after keycloak

--- app/server.js
+++ app/server.js
@@ -76,7 +76,7 @@ app.get('/get-profile', function (req, res) {
 });
 // this is for jenkins commit test
 //2nd try
-//last webhook test1223
+//last webhook after keycloak
 
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
